8e89a07abfd50acdbf4e2b43eca93e9ae8add40b
thiagoheron <thiagoheronavila@gmail.com>
2021-04-12 23:24:38 -0300

    Removendo inputs das imagens na main.m

--- Tarefa3/main.m
+++ Tarefa3/main.m
@@ -3,40 +3,24 @@ fprintf("[Opções de Carregamento de Imagem]\n");
 fprintf("---------------------------------\n");
 fprintf("0 - Digite o Nome da Imagem\n");
 fprintf("1 - Imagem texto_xerox.jpg\n");
-fprintf("2 - Imagem lena.jpg\n");
-fprintf("3 - Imagem lena_sal_e_pimenta.jpg\n");
-fprintf("4 - Imagem lena_cinza.bmp\n");
-fprintf("5 - Imagem Fig0241.tif\n");
-fprintf("6 - Imagem Fig0241_conv.png\n");
-fprintf("7 - Imagem Fig0232a.tif\n");
-fprintf("8 - Imagem Fig0228b.png\n");
-fprintf("9 - Imagem cameraman.tif\n")
-fprintf("10 - Imagem barbara_gray.bmp\n");
+fprintf("2 - Imagem lena_sal_e_pimenta.jpg\n");
+fprintf("3 - Imagem lena_cinza.bmp\n");
+fprintf("4 - Imagem Fig0241_conv.png\n");
+fprintf("5 - Imagem barbara_gray.bmp\n");
 numberSelectImage = input("Digite a Opção da Imagem: ");
 switch numberSelectImage
          case 0 
-                  numberSelectImage = input("Digite Nome da Imagem: ");
-                  inputImage = imread(numberSelectImage);
+                  nameImage = input("Digite Nome da Imagem: ");
          case 1
-                  inputImage = imread('images/input/texto_xerox.jpg');
+                  nameImage = 'images/input/texto_xerox.jpg'
          case 2
-                  inputImage = imread('images/input/lena.png');
+                  nameImage = 'images/input/lena_sal_e_pimenta.jpg'
          case 3
-                  inputImage = imread('images/input/lena_sal_e_pimenta.jpg');
+                  nameImage = 'images/input/lena_cinza.bmp'
          case 4
-                  inputImage = imread('images/input/lena_cinza.bmp');
+                  nameImage = 'images/input/Fig0241_conv.png'
          case 5
-                  inputImage = imread('images/input/Fig0241.tif');
-         case 6
-                  inputImage = imread('images/input/Fig0241_conv.png');
-         case 7
-                  inputImage = imread('images/input/Fig0232a.tif');
-         case 8
-                  inputImage = imread('images/input/Fig0228b.png');
-         case 9
-                  inputImage = imread('images/input/cameraman.tif');
-         case 10
-                  inputImage = imread('images/input/barbara_gray.bmp');
+                  nameImage = 'images/input/barbara_gray.bmp'
          otherwise
                   fprintf("Opção Inválida");
 endswitch
@@ -51,12 +35,12 @@ numberOperation = input("Digite o Número da Operação: ");
 if numberOperation == 1
          paramC = input("Digite o Parâmetro c: ")
          paramGama = input("Digite o Parâmetro gama(γ): ")
-         outputImage = corr_gama(inputImage, paramC, paramGama);
+         outputImage = corr_gama(nameImage, paramC, paramGama);
 
 elseif numberOperation == 2
          paramCV = input("Digite o Parâmetro Cisalhamento Vertical (cv): ")
          paramCH = input("Digite o Parâmetro Cisalhamento Horizontal (ch): ")
-         outputImage = cisalhamento(inputImage, paramCV, paramCH);
+         outputImage = cisalhamento(nameImage, paramCV, paramCH);
 
 endif
 
