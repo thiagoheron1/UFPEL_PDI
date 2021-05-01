3dbd2f1bd9c775523471d24ebc61e8de789000a1
thiagoheron <thiagoheronavila@gmail.com>
2021-05-01 17:12:09 -0300

    Adicionando scripts da tarefa6

---
+++ Tarefa6/main.m
@@ -0,0 +1,10 @@
+# [Input]
+filename = "images/input/lena_cinza.bmp";
+
+# 1 - Passa Alta
+# 2 - Passa Baixa
+# 3 - Elimina Bordas Verticais
+# 4 - Rejeita Faixa
+# 5 - Passa Faixa
+
+filtra_freq(filename, 5)
