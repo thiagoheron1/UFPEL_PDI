d1569ff248c3085493b8df29db3cf81bc988f6e3
thiagoheron <thiagoheronavila@gmail.com>
2021-05-14 09:45:53 -0300

    descrição tste

---
+++ ThiagoHeronAvila_Tarefa7/Quantiza.m
@@ -0,0 +1,26 @@
+function result = Quantiza(entrada, saida, bitsY, bitsCb, bitsCr)
+  imageInput = imread(entrada);
+
+  YCbCr = rgb2ycbcr(imageInput);
+
+  Y = YCbCr(:, :, 1);
+  Cb = YCbCr(:, :, 2);
+  Cr = YCbCr(:, :, 3);
+
+  QY = (Y.*bitsY);
+  QCb = (Cb.*bitsCb);
+  QCr = (Cr.*bitsCr);
+
+  imageQuantizada(:, :, 1) = QY;
+  imageQuantizada(:, :, 2) = QCb;
+  imageQuantizada(:, :, 3) = QCr;
+
+  result = ycbcr2rgb(imageQuantizada);
+
+  figure,imshow(imageInput); title("Image RGB");
+  figure,imshow(YCbCr); title("Image YCbCr");
+
+  imwrite(YCbCr, saida);
+  
+  return;
+endfunction
---
+++ ThiagoHeronAvila_Tarefa7/main.m
@@ -0,0 +1,5 @@
+# [Exercício 2]
+restaura_mediana("images/input/lena_sal_e_pimenta.jpg", 3);
+
+# [Exercício 3]
+#Quantiza("images/input/lena.png", "images/output/imageYCbCr.png", 8, 2, 2);
